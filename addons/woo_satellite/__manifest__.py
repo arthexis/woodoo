d090b7b8c82f4dd52ac9a3e244355b375478e25e
R. J. Guillén-Osorio <arthexis@gmail.com>
2023-06-12 17:06:21 -0600

    removed search, added connection test

--- addons/woo_satellite/__manifest__.py
+++ addons/woo_satellite/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'WooSatellite',
-    'version': '1.0.0',
+    'version': '1.0.1',
     'category': 'Tools',
     'summary': 'Simple WooCommerce integration for Odoo',
     'sequence': 10,
--- addons/woo_satellite/models/satellite.py
+++ addons/woo_satellite/models/satellite.py
@@ -1,4 +1,5 @@
 from odoo import models, fields
+from woocommerce import API
 
 
 # Woo Satellite holds the configuration for each WooCommerce store.
@@ -7,9 +8,21 @@ from odoo import models, fields
 class WooSatellite(models.Model):
     _name = 'woo_satellite.satellite'
     _description = 'WooCommerce Satellite'
-    _rec_name = 'woo_url'
 
     woo_url = fields.Char(string='WooCommerce URL', required=True)
     woo_consumer_key = fields.Char(string='WooCommerce Consumer Key', required=True)
     woo_consumer_secret = fields.Char(string='WooCommerce Consumer Secret', required=True)
+    woo_test_ok = fields.Boolean(string='Connection Test OK?', default=False)
+
+    # Add a button to test the connection to the WooCommerce store.
+    def test_connection(self):
+        for record in self:
+            wcapi = API(
+                url=record.woo_url,
+                consumer_key=record.woo_consumer_key,
+                consumer_secret=record.woo_consumer_secret,
+                version="wc/v3"
+            )
+            wcapi.get("products")
+            record.write({'woo_test_ok': True})
 
