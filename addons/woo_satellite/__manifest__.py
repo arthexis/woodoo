dfc3bef660817d4df9c74c008bb0687ae4f60f96
R. J. Guillén-Osorio <arthexis@gmail.com>
2023-06-13 12:41:18 -0600

    added image to product

--- addons/woo_satellite/__manifest__.py
+++ addons/woo_satellite/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'WooSatellite',
-    'version': '1.0.27',
+    'version': '1.0.28',
     'category': 'Tools',
     'summary': 'Simple WooCommerce integration for Odoo',
     'sequence': 10,
