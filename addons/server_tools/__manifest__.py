c9d7007c17e44ef9e79273d240d2e6f825a50005
R. J. Guillén-Osorio <arthexis@gmail.com>
2023-06-25 14:29:00 -0600

    server run command

--- addons/server_tools/__manifest__.py
+++ addons/server_tools/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Server Tools',
-    'version': '1.0.14',
+    'version': '1.0.15',
     'category': 'Tools',
     'summary': 'Tools for managing Odoo servers',
     'sequence': 8,
--- addons/server_tools/models/servers.py
+++ addons/server_tools/models/servers.py
@@ -13,31 +13,47 @@ class Server(models.Model):
         string='Host', required=True,
     )
     port = fields.Integer(
-        string='Port', required=True,
+        string='Port', required=True, default=22,
     )
     user = fields.Char(
-        string='User', required=True,
+        string='User', required=False,
     )
     password = fields.Char(
-        string='Password', required=True,
+        string='Password', required=False,
+    )
+    pem_key = fields.Text(
+        string='PEM Key', required=False,
     )
     application_ids = fields.One2many(
         string='Applications', comodel_name='server_tools.application',
         inverse_name='server_id',
     )
 
+    # This field is used to store the last command executed on the server
+    command = fields.Char(
+        string='Command', required=False,
+    )
+
     # Get SSH connection
     def get_ssh(self):
         ssh = SSHClient()
         ssh.set_missing_host_key_policy(AutoAddPolicy())
-        ssh.connect(
-            hostname=self.host, port=self.port, username=self.user,
-            password=self.password,
-        )
+        if self.pem_key:
+            ssh.connect(
+                hostname=self.host, port=self.port, username=self.user,
+                key_filename=self.pem_key,
+            )
+        else:
+            ssh.connect(
+                hostname=self.host, port=self.port, username=self.user,
+                password=self.password,
+            )
         return ssh
 
     # Run command
-    def run_command(self, command):
+    def run_command(self, command=None):
+        if not command:
+            command = self.command
         ssh = self.get_ssh()
         stdin, stdout, stderr = ssh.exec_command(command)
         return stdout.read().decode()
@@ -59,3 +75,42 @@ class Application(models.Model):
     password = fields.Char(
         string='Password', required=True,
     )
+
+
+class Database(models.Model):
+    _name = 'server_tools.database'
+    _description = 'Database'
+
+    name = fields.Char(
+        string='Name', required=True,
+    )
+    application_id = fields.Many2one(
+        string='Application', comodel_name='server_tools.application',
+    )
+    ip_address = fields.Char(
+        string='IP Address', required=True,
+    )
+    user = fields.Char(
+        string='User', required=True,
+    )
+    password = fields.Char(
+        string='Password', required=True,
+    )
+
+    # This field is used to store the last SQL query executed on the database
+    sql = fields.Text(
+        string='SQL', required=False,
+    )
+
+    # Run SQL query
+    def run_sql(self, query=None):
+        if not query:
+            query = self.sql
+        ssh = self.application_id.server_id.get_ssh()
+        command = 'psql -h %s -U %s -d %s -c "%s"' % (
+            self.ip_address, self.user, self.name, query,
+        )
+        stdin, stdout, stderr = ssh.exec_command(command)
+        return stdout.read().decode()
+
+    
