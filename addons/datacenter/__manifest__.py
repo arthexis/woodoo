cede0198425ec5ddd3687c83c0b0430d5561d173
R. J. Guillén-Osorio <arthexis@gmail.com>
2023-06-26 11:44:04 -0600

    models

--- addons/datacenter/__manifest__.py
+++ addons/datacenter/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Datacenter',
-    'version': '1.1.10',
+    'version': '1.1.11',
     'category': 'Tools',
     'summary': 'Tools for managing the Enterprise Datacenter',
     'sequence': 8,
