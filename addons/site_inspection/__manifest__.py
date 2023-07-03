c1df6db4a6e1c9193d772093aa3482fe5f0004a4
R. J. Guillén-Osorio <arthexis@gmail.com>
2023-07-03 16:26:52 -0600

    fix

--- addons/site_inspection/__manifest__.py
+++ addons/site_inspection/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Site Inspection',
-    'version': '1.0.5',
+    'version': '1.0.6',
     'category': 'Productivity',
     'summary': 'Store on-site inspection data to generate reports and estimates.',
     'sequence': 9,
