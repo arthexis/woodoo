3144bd8619ab449bd4f8c6f18b6fafb2d24ac189
R. J. Guillén-Osorio <arthexis@gmail.com>
2023-07-05 19:34:51 -0600

    fix

--- addons/datacenter/__manifest__.py
+++ addons/datacenter/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Datacenter',
-    'version': '1.2.8',
+    'version': '1.2.9',
     'category': 'Tools',
     'summary': 'Tools for managing the Enterprise Datacenter',
     'sequence': 8,
