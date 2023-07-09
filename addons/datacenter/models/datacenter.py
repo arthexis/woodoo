b029574577725c4cbecdfc5502e33c7710da6f97
R. J. Guillén-Osorio <arthexis@gmail.com>
2023-07-09 14:33:18 -0600

    context

--- addons/datacenter/models/datacenter.py
+++ addons/datacenter/models/datacenter.py
@@ -110,7 +110,8 @@ class AppServer(models.Model):
         if not command:
             command = self.command
         try:
-            resolved = sigils.Sigil(command) % self
+            with sigils.Context(self):
+                resolved = sigils.Sigil(command) % None
             _logger.info('Resolved command: %s' % resolved)
             return resolved
         except Exception as e:
