6aec1e49736e26bd385bf4d302a94cc160411bf1
R. J. Guillén-Osorio <arthexis@gmail.com>
2023-07-10 15:09:06 -0600

    even more fix

--- addons/datacenter/models/datacenter.py
+++ addons/datacenter/models/datacenter.py
@@ -14,7 +14,7 @@ class DefaultFieldMixin(models.AbstractModel):
     def write(self, values):
         for field_name, field in self._fields.items():
             # Only override if the field currently has no value in self
-            if field.default and getattr(self, field_name) is None:
+            if field.default and values.get(field_name) is None:
                 values[field_name] = field.default(self)
         return super().write(values)
 
