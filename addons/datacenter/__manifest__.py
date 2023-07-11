570cbf663dbaeca48e894ce79bd66fd614c00d65
R. J. Guillén-Osorio <arthexis@gmail.com>
2023-07-11 14:47:23 -0600

    smtgh

--- addons/datacenter/__manifest__.py
+++ addons/datacenter/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Datacenter',
-    'version': '1.4.6',
+    'version': '1.4.7',
     'category': 'Tools',
     'summary': 'Tools for managing the Enterprise Datacenter',
     'sequence': 8,
