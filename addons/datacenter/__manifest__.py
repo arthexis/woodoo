1aea7ecb04ce7e7782b6b4074890ee049a488a68
R. J. Guillén-Osorio <arthexis@gmail.com>
2023-07-10 19:18:49 -0600

    appcode field

--- addons/datacenter/__manifest__.py
+++ addons/datacenter/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Datacenter',
-    'version': '1.4.1',
+    'version': '1.4.2',
     'category': 'Tools',
     'summary': 'Tools for managing the Enterprise Datacenter',
     'sequence': 8,
