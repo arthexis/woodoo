9b1551ee9f3c7d5a4be604d70b57355410aebb16
R. J. Guillén-Osorio <arthexis@gmail.com>
2023-07-24 09:57:55 -0600

    planner notes

--- addons/site_inspection/__manifest__.py
+++ addons/site_inspection/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Site Inspection',
-    'version': '1.1.9',
+    'version': '1.1.10',
     'category': 'Productivity',
     'summary': 'Store on-site inspection data to generate reports and estimates.',
     'sequence': 9,
